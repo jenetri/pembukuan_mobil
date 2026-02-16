7f3a9ab8aa1191e0a1682d0ee2b9992a03a2c7d3
jenetri <jenetri@yahoo.com>
2026-02-16 07:39:29 +0700

    fix pwa config

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,9 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: "autoUpdate",
+      devOptions: {
+        enabled: true,
+      },
       manifest: {
         name: "Pembukuan Mobil",
         short_name: "Pembukuan",
